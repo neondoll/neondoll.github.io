03efacd2b65b27554150eedb3020489b0ed61a08
neondoll <bonni_1998@mail.ru>
2023-11-17 01:55:16 +0300

    Add Vite + Vue + TS

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import ThemeCheckbox from "./components/ThemeCheckbox.vue";
+import developer from "./assets/images/NxcQR6VIBiA.jpg";
+</script>
+
+<template>
+  <header class="header">
+    <div class="header__container container">
+      <ThemeCheckbox/>
+    </div>
+  </header>
+  <main class="main container">
+    <section class="hero">
+      <div class="hero__text-container">
+        <h2 class="hero__title">Привет, я Александра!</h2>
+        <p class="hero__subtitle">Будем знакомы</p>
+      </div>
+      <div class="hero__image-container">
+        <div class="hero__image"></div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
